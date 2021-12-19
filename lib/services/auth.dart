361b7f096b68e0d3ce2f3cd1933b416d8ff86c8d
Metin Yıldıran <metin.1464@gmail.com>
2021-12-19 19:34:13 +0300

    Added User class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,20 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user object based on FirebaseUser
+  LocalUser? _userFromFirebase(User user) {
+    return user != null ? LocalUser(uid: user.uid) : null;
+  }
 
   // sign in anonymously
   Future signInAnon() async {
     try{
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
+      return _userFromFirebase(user!);
     }catch(e){
       print(e.toString());
       return null;
